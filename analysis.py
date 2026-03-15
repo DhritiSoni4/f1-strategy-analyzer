088823aafae1e9747695bc187a877bcfcdcaf2a7
Dhriti Soni <dhritisoni@Dhritis-MacBook-Pro.local>
2026-03-15 16:02:59 +0530

    Add lap time delta analysis between drivers

--- analysis.py
+++ analysis.py
@@ -1,29 +1,31 @@
 import fastf1
 import matplotlib.pyplot as plt
+import pandas as pd
 
 fastf1.Cache.enable_cache('./cache')
 
-# load race
 session = fastf1.get_session(2024, "Monaco", "R")
 session.load()
 
-# get laps for two drivers
 ver_laps = session.laps.pick_drivers("VER")
 ham_laps = session.laps.pick_drivers("HAM")
 
 # convert lap times
-ver_times = ver_laps['LapTime'].dt.total_seconds()
-ham_times = ham_laps['LapTime'].dt.total_seconds()
+ver_times = ver_laps['LapTime'].dt.total_seconds().reset_index(drop=True)
+ham_times = ham_laps['LapTime'].dt.total_seconds().reset_index(drop=True)
+
+# calculate delta
+delta = ham_times - ver_times
+
+laps = range(1, len(delta)+1)
 
 plt.figure(figsize=(10,5))
+plt.plot(laps, delta)
 
-plt.plot(ver_laps['LapNumber'], ver_times, label="Verstappen")
-plt.plot(ham_laps['LapNumber'], ham_times, label="Hamilton")
+plt.axhline(0)
 
 plt.xlabel("Lap Number")
-plt.ylabel("Lap Time (seconds)")
-plt.title("Driver Pace Comparison - Monaco GP")
-
-plt.legend()
+plt.ylabel("Time Difference (Hamilton - Verstappen)")
+plt.title("Lap Time Delta: HAM vs VER - Monaco GP")
 
 plt.show()
