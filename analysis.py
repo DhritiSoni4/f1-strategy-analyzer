f6486f96b045301c0fd01142831ccd3ca7dc808c
Dhriti Soni <dhritisoni@Dhritis-MacBook-Pro.local>
2026-03-15 15:57:08 +0530

    Add driver pace comparison analysis

--- analysis.py
+++ analysis.py
@@ -3,18 +3,27 @@ import matplotlib.pyplot as plt
 
 fastf1.Cache.enable_cache('./cache')
 
+# load race
 session = fastf1.get_session(2024, "Monaco", "R")
 session.load()
 
-driver_laps = session.laps.pick_drivers("VER")
+# get laps for two drivers
+ver_laps = session.laps.pick_drivers("VER")
+ham_laps = session.laps.pick_drivers("HAM")
 
-lap_times = driver_laps['LapTime'].dt.total_seconds()
+# convert lap times
+ver_times = ver_laps['LapTime'].dt.total_seconds()
+ham_times = ham_laps['LapTime'].dt.total_seconds()
 
 plt.figure(figsize=(10,5))
-plt.plot(driver_laps['LapNumber'], lap_times)
+
+plt.plot(ver_laps['LapNumber'], ver_times, label="Verstappen")
+plt.plot(ham_laps['LapNumber'], ham_times, label="Hamilton")
 
 plt.xlabel("Lap Number")
 plt.ylabel("Lap Time (seconds)")
-plt.title("Max Verstappen Lap Times - Monaco GP")
+plt.title("Driver Pace Comparison - Monaco GP")
+
+plt.legend()
 
 plt.show()
