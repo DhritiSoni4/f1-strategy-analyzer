dd3d8ee453ec2c6ed7e16a5b702baa87549987ed
Dhriti Soni <dhritisoni4@gmail.com>
2026-04-08 18:16:34 +0530

    New frontend code using React app

---
+++ f1-frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ display: "flex", height: "100vh", backgroundColor: "#111", color: "white" }}>
+      
+      {/* Sidebar */}
+      <div style={{ width: "25%", backgroundColor: "#1a1a1a", padding: "15px" }}>
+        <h2>Race Setup</h2>
+      </div>
+
+      {/* Main Area */}
+      <div style={{ flex: 1, padding: "20px" }}>
+        <h1>F1 Strategy Dashboard</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
